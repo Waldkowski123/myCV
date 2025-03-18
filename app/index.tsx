d8b381a882a0925bfaee0f097fe6b8caed418970
Valentin Banobre Kalinowski <valentin.banobre-kalinowski@infotech.edu.pl>
2025-03-18 14:22:47 +0100

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-yellow-200 justify-center items-center">
+      <Text className="font-bold text-[32px]">Hi! My Name is Valentin</Text>
+    </View>
+  );
+}
